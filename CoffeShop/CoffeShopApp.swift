9031c7b63a8bd5f07cb0e0bb9ddc6763dff630cc
zaki.m.zaki <zaki.m.zaki@te.eg>
2024-08-05 11:37:53 +0300

    Initial Commit

---
+++ CoffeShop/CoffeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopApp.swift
+//  CoffeShop
+//
+//  Created by Zaki on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
