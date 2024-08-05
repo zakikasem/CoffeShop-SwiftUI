1065e2b4f88664f958ae137789d8516b80671080
zaki.m.zaki <zaki.m.zaki@te.eg>
2024-08-05 11:59:36 +0300

    Make project accept with MVVM structure commit

---
+++ CoffeShop/Modules/Coffe Details Screen/view/CoffeDetailsView.swift
@@ -0,0 +1,23 @@
+//
+//  CoffeDetailsView.swift
+//  CoffeShop
+//
+//  Created by Zaki on 05/08/2024.
+//
+
+import SwiftUI
+
+struct CoffeDetailsView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+    
+}
+
+struct CoffeDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoffeDetailsView()
+    }
+}
